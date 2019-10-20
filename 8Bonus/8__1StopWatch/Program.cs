cd8127696dff806a078e6420bc28cbdd48d52151
saowalak2542 <56579726+saowalak2542@users.noreply.github.com>
2019-10-21 03:12:37 +0700

    8_1 Update

--- 8Bonus/8__1StopWatch/Program.cs
+++ 8Bonus/8__1StopWatch/Program.cs
@@ -4,15 +4,53 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
+
 namespace StopWatch
 {
-    class Program
+   class Program
     {
         static void Main(string[] arg)
         {
-            
+            var stopwatch = new Stopwatch();
+
+            for (int i = 0; i < 2; i++)
+            {
+                stopwatch.start(DateTime.Now);
+
+                for (int j = 0; j < 1000; j++)
+                {
+                    Thread.Sleep(1);
+                }
+
+                stopwatch.stop(DateTime.Now);
+                Console.WriteLine(stopwatch.GetInterval().ToString());
+                Console.ReadLine();
+            }
         }
 
     }
-      
+    public class Stopwatch
+    {
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+
+        private bool running = false;
+
+        public void start(DateTime start)
+        {
+            if (!running)
+            {
+                EndTime = stop;
+                running = false;
+            }
+        }
+        public TimeSpan GetInterval()
+        {
+            var duration = EndTime - StartTime;
+            return duration;
+        }
+    }
+
+
 }
