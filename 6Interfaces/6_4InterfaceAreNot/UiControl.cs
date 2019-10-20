b3e717b613e7af64f5644158835943b07eac45d3
saowalak2542 <56579726+saowalak2542@users.noreply.github.com>
2019-10-21 02:10:54 +0700

    6_4 UpdateUiControl

--- 6Interfaces/6_4InterfaceAreNot/UiControl.cs
+++ 6Interfaces/6_4InterfaceAreNot/UiControl.cs
@@ -1,8 +1,21 @@
-﻿namespace InterfaceAndInheritance
+﻿using System;
+
+namespace InterfaceAndInheritance
 {
     public class UiControl
     {
+        public string Id { get; set; }
+        public Size Size { get; set; }
+        public Position TopLeft { get; set; }
 
+        public virtual void Draw()
+        {
+
+        }
+        public void Focus()
+        {
+            Console.WriteLine("Received focus.");
+        }
     }
       
 }
