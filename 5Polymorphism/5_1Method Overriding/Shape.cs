2d0e636e6034685f6c82387f69c56cc059bc53b0
saowalak2542 <56579726+saowalak2542@users.noreply.github.com>
2019-10-20 22:27:00 +0700

    5_1 UpdateShape

--- 5Polymorphism/5_1Method Overriding/Shape.cs
+++ 5Polymorphism/5_1Method Overriding/Shape.cs
@@ -2,7 +2,14 @@
 {
     public class Shape
     {
+        public int Width { get; set; }
+        public int Height { get; set; }
+        public Position Position { get; set; }
 
+        public virtual void Draw()
+        {
+
+        }
     }
       
 }
