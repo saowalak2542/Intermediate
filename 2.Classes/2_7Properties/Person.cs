3d85d78dd26a4be61112a88ad8fedd5fc8b0bcde
saowalak2542 <56579726+saowalak2542@users.noreply.github.com>
2019-10-20 19:27:46 +0700

    2_7UpdatePerson

--- 2.Classes/2_7Properties/Person.cs
+++ 2.Classes/2_7Properties/Person.cs
@@ -1,8 +1,28 @@
-﻿namespace Properties
+﻿using System;
+
+namespace Properties
 {
+
     public class Person
     {
+        public string Name { get; set; }
+        public string Username { get; set; }
+        public DateTime Birthdate { get; private set; }
+        public Person(DateTime birthdate)
+        {
+            Birthdate = birthdate;
+        }
+        public int Age
+        {
+            get
+            {
+                var timespan = DateTime.Today - Birthdate;
+                var years = timespan.Days / 365;
 
+                return years;
+            }
+        }
     }
+    
     
 }
