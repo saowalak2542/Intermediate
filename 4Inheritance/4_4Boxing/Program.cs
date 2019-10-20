56ca4e02fcdcc986598e4f87b5e29c6161932eba
saowalak2542 <56579726+saowalak2542@users.noreply.github.com>
2019-10-20 22:13:41 +0700

    4_4 CreateFile

---
+++ 4Inheritance/4_4Boxing/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Boxing
+{
+    partial class Program
+    {
+        static void Main(string[] arg)
+        {
+          
+        }
+
+    }
+      
+}
