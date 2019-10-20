5e61daf3452e26c83c56f4cd1ed62eb3986830f5
saowalak2542 <56579726+saowalak2542@users.noreply.github.com>
2019-10-20 21:05:31 +0700

    4_1 UpdateCustomer

--- 4Inheritance/4_1AccessModifiers/Customer.cs
+++ 4Inheritance/4_1AccessModifiers/Customer.cs
@@ -4,25 +4,18 @@ namespace AccessModifiers
 {
     public class Customer
     {
-        public int Id { get; set; }
+       
         public string Name { get; set; }
         public void Promote()
         {
-            var rating = CalculateRating(excludeOrders: true);
-            if (rating == 0)
-                Console.WriteLine("Promoted tp Level 1");
-            else
-                Console.WriteLine("Promoted tp Level 2");
+            var calculator = new RateCalculator();
+            var rating = calculator.Calculate(this);
+
+            Console.WriteLine("Promote logic changed.");
 
         }
-        private int CalculateRating(bool excludeOrders)
-        {
-            return 0;
-        }
+        
+       
     }
   
-    
-         
-
-
 }
