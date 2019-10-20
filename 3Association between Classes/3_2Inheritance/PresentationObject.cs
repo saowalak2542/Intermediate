37b0704b2c23b3ca40aefb6afac8b8e358bd6a3e
saowalak2542 <56579726+saowalak2542@users.noreply.github.com>
2019-10-20 20:03:23 +0700

    3_2 UpdatePresentationObject

--- 3Association between Classes/3_2Inheritance/PresentationObject.cs
+++ 3Association between Classes/3_2Inheritance/PresentationObject.cs
@@ -1,8 +1,20 @@
-﻿namespace Inheritance
-{
+﻿using System;
+
+namespace Inheritance
+{ 
     public class PresentationObject
     {
+        public int Width { get; set; }
+        public int Height { get; set; }
 
+        public void Copy()
+        {
+            Console.WriteLine("Object copied to clipboard");
+        }
+        public void Duplicate()
+        {
+            Console.WriteLine("Objecy was duplicated.");
+        }
     }
      
 }
