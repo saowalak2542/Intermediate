cb5d3b868a7cf86698eeb65afde8e2c4b47b4994
saowalak2542 <56579726+saowalak2542@users.noreply.github.com>
2019-10-20 20:09:23 +0700

    3_2UpdateText

--- 3Association between Classes/3_2Inheritance/Text.cs
+++ 3Association between Classes/3_2Inheritance/Text.cs
@@ -1,8 +1,14 @@
 ﻿namespace Inheritance
 {
-    public class Text
+    public class Text : PresentationObject
     {
+        public int FontSize { get; set; }
+        public string FontName { get; set; }
 
+        public void AddHyperlink(string url)
+        {
+            System.Console.WriteLine("We added a link to " + url);
+        }
     }
      
 }
