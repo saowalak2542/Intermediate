a42fab80a077079d27324ad5f3238d1da4080dd8
saowalak2542 <56579726+saowalak2542@users.noreply.github.com>
2019-10-20 21:25:09 +0700

    4_2 UpdateCar

--- 4Inheritance/4_2Constructors/Car.cs
+++ 4Inheritance/4_2Constructors/Car.cs
@@ -1,10 +1,14 @@
-﻿namespace Constructors
-{
-    public class Car
-    {
+﻿using System;
 
+namespace Constructors
+{
+    public class Car : Vehicle
+    {
+        public Car(string registrationNumber)
+              : base(registrationNumber)
+        {
+            Console.WriteLine("Car is being initialized. {0}", registrationNumber);
+        }
     }
-    
-    
+
 }
-     
