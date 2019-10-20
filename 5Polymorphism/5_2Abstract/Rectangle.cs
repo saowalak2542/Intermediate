6a9526b7f512b2049f671cff9918aa972e5cb5c5
saowalak2542 <56579726+saowalak2542@users.noreply.github.com>
2019-10-20 22:58:16 +0700

    5_2 UpdateRectangle

--- 5Polymorphism/5_2Abstract/Rectangle.cs
+++ 5Polymorphism/5_2Abstract/Rectangle.cs
@@ -1,8 +1,14 @@
-﻿namespace Abstract
-{
-    public class Rectangle
-    {
+﻿using System;
 
+namespace Abstract
+{
+    public class Rectangle : Shape
+    {
+        public override void Draw()
+        {
+            Console.WriteLine("Draw a rectangle");
+        }
     }
+    
       
 }
