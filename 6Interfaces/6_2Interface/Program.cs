4725b715b7c87320742f918b5c52716333415cd1
saowalak2542 <56579726+saowalak2542@users.noreply.github.com>
2019-10-20 23:35:31 +0700

    6_2 UpdateMain

--- 6Interfaces/6_2Interface/Program.cs
+++ 6Interfaces/6_2Interface/Program.cs
@@ -10,7 +10,9 @@ namespace Testability
     {
         static void Main(string[] arg)
         {
-
+            var orderProcessor = new OrderProcessor(new ShippingCalculator());
+            var order = new Order { DatePlaced = DateTime.Now, TotalPrice = 100f };
+            orderProcessor.Process(order);
         }
 
     }
