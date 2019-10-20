aa917d50ab0faec80ecf0884bc777e5b880df411
saowalak2542 <56579726+saowalak2542@users.noreply.github.com>
2019-10-20 22:38:54 +0700

    5_1 Finish

--- 5Polymorphism/5_1Method Overriding/Program.cs
+++ 5Polymorphism/5_1Method Overriding/Program.cs
@@ -10,7 +10,12 @@ namespace MethodOverriding
     {
         static void Main(string[] arg)
         {
-           
+            var shapes = new List<Shape>();
+            shapes.Add(new Circle());
+            shapes.Add(new Rectangle());
+
+            var canvas = new Canvas();
+            canvas.DrawShapes(shapes);
         }
 
     }
