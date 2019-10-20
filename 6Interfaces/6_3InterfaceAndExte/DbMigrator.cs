34128d2adf0979450c546cba0ae1eb925301c5a4
saowalak2542 <56579726+saowalak2542@users.noreply.github.com>
2019-10-21 01:44:12 +0700

    6_3 UpdateFileLogger

--- 6Interfaces/6_3InterfaceAndExte/DbMigrator.cs
+++ 6Interfaces/6_3InterfaceAndExte/DbMigrator.cs
@@ -1,8 +1,24 @@
-﻿namespace Extensiblity
+﻿using System;
+
+namespace Extensiblity
 {
     public class DbMigrator
     {
+        private readonly ILogger _logger;
+        public DbMigrator(ILogger logger)
+        {
+            _logger = logger;
+        }
+        public void Migrate()
+        {
+            _logger.LogInfo("Migration started at {0}" + DateTime.Now);
 
+            // Details of migrating the datebase
+
+
+
+            _logger.LogInfo("Migration finished at {0}" + DateTime.Now);
+        }
     }
       
 }
