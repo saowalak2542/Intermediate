bfea5412670d232ebcefbeeda9d158f12aae5e86
saowalak2542 <56579726+saowalak2542@users.noreply.github.com>
2019-10-20 16:44:02 +0700

    2_2_Finish

--- 2.Classes/2_2Constructors/Program.cs
+++ 2.Classes/2_2Constructors/Program.cs
@@ -10,7 +10,15 @@ namespace Constructors
     {
         static void Main(string[] arg)
         {
+            var customer = new Customer();
+            customer.Id = 1;
+            customer.Name = "John";
 
+            var order = new Order();
+            customer.Orders.Add(order);
+
+            Console.WriteLine(customer.Id);
+            Console.WriteLine(customer.Name);
         }
     }
 }
