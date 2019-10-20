a1885cb588fa5b6b8a9758f1073dc5a1dbb04f2e
saowalak2542 <56579726+saowalak2542@users.noreply.github.com>
2019-10-20 20:33:16 +0700

    3_3UpdateInstaller

--- 3Association between Classes/3_3Composition/Installer.cs
+++ 3Association between Classes/3_3Composition/Installer.cs
@@ -2,7 +2,15 @@
 {
     public class Installer
     {
-
+        private readonly Logger _logger;
+        public Installer(Logger logger)
+        {
+            _logger = logger;
+        }
+        public void Install()
+        {
+            _logger.Log("We are installing the cpplication.");
+        }
     }
      
 }
