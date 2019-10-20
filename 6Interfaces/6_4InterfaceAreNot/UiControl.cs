0d6c30455eaa09372e4ed151ba5c1d874d45c237
saowalak2542 <56579726+saowalak2542@users.noreply.github.com>
2019-10-21 02:16:05 +0700

    6_4 Finish

--- 6Interfaces/6_4InterfaceAreNot/UiControl.cs
+++ 6Interfaces/6_4InterfaceAreNot/UiControl.cs
@@ -2,20 +2,25 @@
 
 namespace InterfaceAndInheritance
 {
+    public class TextBox : UiControl, IDraggable, IDroppable
+    {
+        public void Drag()
+        {
+            throw new NotImplementedException();
+            {
+
+            }
+        }
+        public void Drop()
+        {
+            throw new NotImplementedException();
+        }
+    }
     public class UiControl
     {
         public string Id { get; set; }
-        public Size Size { get; set; }
-        public Position TopLeft { get; set; }
-
-        public virtual void Draw()
-        {
-
-        }
-        public void Focus()
-        {
-            Console.WriteLine("Received focus.");
-        }
     }
-      
+
+
+
 }
