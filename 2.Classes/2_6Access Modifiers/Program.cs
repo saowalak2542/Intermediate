968315eb35195ce7a32f89d2aefb21aca3dcd519
saowalak2542 <56579726+saowalak2542@users.noreply.github.com>
2019-10-20 18:14:37 +0700

    2_6UpdateProgram

--- 2.Classes/2_6Access Modifiers/Program.cs
+++ 2.Classes/2_6Access Modifiers/Program.cs
@@ -1,11 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace AccessModifiers
 {
+    public class Person
+    {
+        private DateTime _birthdate;
+
+        public void SetBirthdate(DateTime birthdate)
+        {
+            _birthdate = birthdate;
+        }
+        public DateTime GetBirthdate()
+        {
+            return _birthdate;
+        }
+    }
     class Program
     {
         static void Main(string[] arg)
