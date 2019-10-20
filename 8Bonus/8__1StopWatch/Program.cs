544eb83310e37c2032e3b2355c23bda949fd4b5f
saowalak2542 <56579726+saowalak2542@users.noreply.github.com>
2019-10-21 03:48:18 +0700

    8_1 FixAndUpdate

--- 8Bonus/8__1StopWatch/Program.cs
+++ 8Bonus/8__1StopWatch/Program.cs
@@ -1,58 +1,64 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 
-
 namespace StopWatch
 {
-   class Program
+    class Program
     {
-        static void Main(string[] arg)
+        static void Main(string[] args)
         {
             var stopwatch = new Stopwatch();
 
             for (int i = 0; i < 2; i++)
             {
-                stopwatch.start(DateTime.Now);
-
+                stopwatch.start(DateTime.Today.AddDays(1));
+                
                 for (int j = 0; j < 1000; j++)
                 {
                     Thread.Sleep(1);
                 }
-                stopwatch.StartTime = DateTime.Today.AddDays(1);
-                stopwatch.EndTime = DateTime.Today.AddYears(-1);
 
-                stopwatch.stop(DateTime.Now);
+                stopwatch.stop(DateTime.Today.AddYears(-1));
+                stopwatch.stop(DateTime.Today.AddYears(-1));
                 Console.WriteLine(stopwatch.GetInterval().ToString());
                 Console.ReadLine();
             }
         }
-
     }
     public class Stopwatch
     {
-        public DateTime StartTime { get; set; }
-        public DateTime EndTime { get; set; }
+        private DateTime _startTime;
+        private DateTime _endTime;
 
-        private bool running = false;
+        private bool _running;
 
-        public void start(DateTime start)
+        public void start()
         {
-            if (!running)
-            {
-                EndTime = stop;
-                running = false;
-            }
+            if (_running)
+                throw new InvalidOperationException("Stopwatch is already running");
+        }
+        _startTime = DateTime.Now;
+         _running = true;
+        }
+    public void stop()
+    {
+        if (!_running)
+            throw new InvalidOperationException("Stopwatch is not running");
+
+        _endTime = DateTime.Now;
+        _running = false;
         }
         public TimeSpan GetInterval()
         {
-            var duration = EndTime - StartTime;
-            return duration;
+            return _endTime - _startTime;
+
         }
     }
-
-
+    
 }
+       
