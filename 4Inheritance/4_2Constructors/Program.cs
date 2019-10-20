ce0100314f7ab5d92f46b0f1f0bf73aa5a42e204
saowalak2542 <56579726+saowalak2542@users.noreply.github.com>
2019-10-20 21:09:25 +0700

    4_2CreateFile

---
+++ 4Inheritance/4_2Constructors/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructors
+{
+    class Program
+    {
+        static void Main(string[] arg)
+        {
+           
+        }
+
+    }
+    
+    
+}
+     
