9ba43622122bc35ecd5c302cd8acc31d1ffe8231
saowalak2542 <56579726+saowalak2542@users.noreply.github.com>
2019-10-20 17:23:29 +0700

    2_4UpdateUsePoints

--- 2.Classes/2_4Methods/Program.cs
+++ 2.Classes/2_4Methods/Program.cs
@@ -28,5 +28,21 @@ namespace Methods
 
                 Console.WriteLine("Conversion failed.");
         }
+        static void UsePoints()
+        {
+            try
+            {
+                var point = new Point(10, 20);
+                point.Move(new Point(40, 60));
+                Console.WriteLine("Point is at ({0}, {1})", point.X, point.Y);
+
+                point.Move(100, 200);
+                Console.WriteLine("Point is at ({0}, {1})", point.X, point.Y);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("An unexpected error occured");
+            }
+        }
     }
 }
