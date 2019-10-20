6cb3fb1c9640862569fa549ec27bee4d20bdbdf3
saowalak2542 <56579726+saowalak2542@users.noreply.github.com>
2019-10-21 01:26:35 +0700

    6_3CreateFile

---
+++ 6Interfaces/6_3InterfaceAndExte/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Extensiblity
+{
+    class Program
+    {
+        static void Main(string[] arg)
+        {
+          
+        }
+
+    }
+      
+}
