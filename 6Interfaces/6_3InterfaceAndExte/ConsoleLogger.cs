68e41ad6eab3824ecc82f047aadc589a1b3eee51
saowalak2542 <56579726+saowalak2542@users.noreply.github.com>
2019-10-21 01:48:34 +0700

    6_3 UpdateConsoleLogger

--- 6Interfaces/6_3InterfaceAndExte/ConsoleLogger.cs
+++ 6Interfaces/6_3InterfaceAndExte/ConsoleLogger.cs
@@ -1,8 +1,19 @@
-﻿namespace Extensiblity
-{
-    public class ConsoleLogger
-    {
+﻿using System;
 
+namespace Extensiblity
+{
+    public class ConsoleLogger : ILogger
+    {
+        public void LogError(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(message);
+        }
+        public void LogInfo(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(message);
+        }
     }
       
 }
