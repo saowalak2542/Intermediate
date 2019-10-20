bf4963377d2547d5368c66d51435af5f98b9df00
saowalak2542 <56579726+saowalak2542@users.noreply.github.com>
2019-10-20 23:24:05 +0700

    6_2 UpdateOrder

--- 6Interfaces/6_2Interface/Order.cs
+++ 6Interfaces/6_2Interface/Order.cs
@@ -1,9 +1,18 @@
-﻿namespace Testability
+﻿using System;
+
+namespace Testability
 {
     public class Order
     {
-       
-        
+        public int Id { get; set; }
+        public DateTime DatePlaced { get; set; }
+        public Shipment Shipment { get; set; }
+        public float TotalPrice { get; set; }
+        public bool IsShipped
+        {
+            get { return Shipment != null; }
+        }
+
     }
       
 }
