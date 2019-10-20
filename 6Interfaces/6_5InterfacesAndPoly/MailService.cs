02202ad9e7af5ee52ccaa35ca9c1807161b60c10
saowalak2542 <56579726+saowalak2542@users.noreply.github.com>
2019-10-21 02:38:31 +0700

    6_5 UpdateMailService

--- 6Interfaces/6_5InterfacesAndPoly/MailService.cs
+++ 6Interfaces/6_5InterfacesAndPoly/MailService.cs
@@ -1,7 +1,14 @@
-﻿namespace InterfacePolymor
+﻿using System;
+
+namespace InterfacePolymor
 {
     public class MailService
     {
+        public void Send(Mail mail)
+        {
+            Console.WriteLine("Sending email...");
+        }
+
 
     }
       
