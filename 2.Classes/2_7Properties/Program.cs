ea607807fd09319edc1f7264ec4461a52f89ae77
saowalak2542 <56579726+saowalak2542@users.noreply.github.com>
2019-10-20 19:31:11 +0700

    2_7Finish

--- 2.Classes/2_7Properties/Program.cs
+++ 2.Classes/2_7Properties/Program.cs
@@ -6,13 +6,18 @@ using System.Threading.Tasks;
 
 namespace Properties
 {
-   
+
     class Program
     {
         static void Main(string[] arg)
         {
-           
+            var person = new Person(new DateTime(1982, 1, 1));
+            Console.WriteLine(person.Age);
+
+
+
         }
     }
-    }
+    
+    
 }
